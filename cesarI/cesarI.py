d519f56ede5363d8dc4f8d15973bdf43b7ad88d9
Tomasz Derek <tomasz_derek@wp.pl>
2022-10-02 14:28:50 -0400

    Update cesarI.py

--- cesarI/cesarI.py
+++ cesarI/cesarI.py
@@ -1,11 +1,13 @@
-def decrypt(string, key:int):   
-    decrypted = ""
-    chars = []
-    for char in string:
-        pom = ord(char) - key
-        if pom < 65:
-            pom += 62
-        decrypted += chr(pom)
-    print(decrypted)
+def cesar(string: str, key: int):
+    list_of_strings = string.split(" ")
+    for l in list_of_strings:
+        for char in l:
+            index = ord(char) - ord('A')
+            print(chr(((index - key) % 26) + 65), end = '')
+        print(end='\n')
 
-decrypt('PDA MQEYG XNKSJ BKT FQILO KRAN PDA HWVU ZKC KB YWAOWN WJZ UKQN QJEMQA OKHQPEKJ EO JANBWJJWIDCL', 1)
+
+if __name__ == "__main__":
+    string = "JXU GKYSA RHEMD VEN ZKCFI ELUH JXU BQPO TEW EV SQUIQH QDT OEKH KDYGKU IEBKJYED YI DUHVQDDQCXWF"
+    #  Solution NERFANNAMHGP
+    cesar(string, -10)
